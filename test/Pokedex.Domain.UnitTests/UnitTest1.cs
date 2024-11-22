a36898859a401ab98c8468cdefefa97f8b4e00f1
Enrico Massone <enrico.massone@hotmail.it>
2024-11-22 21:19:53 +0100

    feat: Created repository basic structure

---
+++ test/Pokedex.Domain.UnitTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Pokedex.Domain.UnitTests
+{
+  public class UnitTest1
+  {
+    [Fact]
+    public void Test1()
+    {
+
+    }
+  }
+}
---
+++ test/Pokedex.Infrastructure.UnitTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Pokedex.Infrastructure.UnitTests
+{
+  public class UnitTest1
+  {
+    [Fact]
+    public void Test1()
+    {
+
+    }
+  }
+}
