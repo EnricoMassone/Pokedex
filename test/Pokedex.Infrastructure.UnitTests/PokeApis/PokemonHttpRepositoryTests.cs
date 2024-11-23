889f908478a44f0e2880d5c4e72e97248a5d37dd
Enrico Massone <enrico.massone@hotmail.it>
2024-11-23 18:48:17 +0100

    feat: wip

---
+++ test/Pokedex.Infrastructure.UnitTests/PokeApis/PokemonHttpRepositoryTests.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using Moq;
+using Pokedex.Infrastructure.PokeApis;
+
+namespace Pokedex.Infrastructure.UnitTests.PokeApis;
+
+public sealed class PokemonHttpRepositoryTests
+{
+  private readonly Mock<IPokeApiHttpClient> _httpClientMock;
+  private readonly PokemonHttpRepository _sut;
+
+  public PokemonHttpRepositoryTests()
+  {
+    _httpClientMock = new Mock<IPokeApiHttpClient>(MockBehavior.Strict);
+    _sut = new PokemonHttpRepository(_httpClientMock.Object);
+  }
+
+  [Fact]
+  public async Task GetPokemonByNameAsync_Throws_ArgumentNullException_When_Name_Is_Null()
+  {
+    // ACT
+    var exception = await Assert.ThrowsAsync<ArgumentNullException>(
+      () => _sut.GetByNameAsync(null!, CancellationToken.None)
+    );
+
+    // ASSERT
+    exception.ParamName.Should().Be("name");
+  }
+}
