8814cde71073e5fe8d467c0f796bfd860e72b8db
Enrico Massone <enrico.massone@hotmail.it>
2024-11-23 20:27:24 +0100

    feat: completed tests on class PokemonHttpRepository

--- test/Pokedex.Infrastructure.UnitTests/PokeApis/PokemonHttpRepositoryTests.cs
+++ test/Pokedex.Infrastructure.UnitTests/PokeApis/PokemonHttpRepositoryTests.cs
@@ -95,4 +95,184 @@ public sealed class PokemonHttpRepositoryTests
     result.Should().NotBeNull();
     result.HasValue.Should().BeFalse();
   }
+
+  [Theory]
+  [AutoData]
+  public async Task GetPokemonByNameAsync_Handles_Null_Value_Of_Habitat_Property(
+    string name,
+    bool isLegendary,
+    string flavorText,
+    CancellationToken cancellationToken)
+  {
+    // ARRANGE
+    var flavorTextDto = new FlavorTextDto
+    {
+      FlavorText = flavorText,
+      Language = new LanguageDto { Name = CultureCodes.English },
+    };
+
+    var pokemonApiResponse = new PokemonApiResponse
+    {
+      IsLegendary = isLegendary,
+      Name = name,
+      FlavorTextEntries = [flavorTextDto],
+      Habitat = null,
+    };
+
+    _httpClientMock
+      .Setup(m => m.GetPokemonByNameAsync(new Name(name), cancellationToken))
+      .ReturnsAsync(pokemonApiResponse, TimeSpan.FromMilliseconds(value: 10));
+
+    // ACT
+    var result = await _sut.GetByNameAsync(new Name(name), cancellationToken);
+
+    // ASSERT
+    result.Should().NotBeNull();
+    result.HasValue.Should().BeTrue();
+
+    // check result value
+    var pokemon = result.Value;
+
+    pokemon.Should().NotBeNull();
+    pokemon.Habitat.Should().BeNull();
+  }
+
+  [Theory]
+  [AutoData]
+  public async Task GetPokemonByNameAsync_Handles_Empty_List_Of_FlavorTextEntries(
+    string name,
+    bool isLegendary,
+    string habitat,
+    CancellationToken cancellationToken)
+  {
+    // ARRANGE
+    var habitatDto = new HabitatDto { Name = habitat };
+
+    var pokemonApiResponse = new PokemonApiResponse
+    {
+      IsLegendary = isLegendary,
+      Name = name,
+      FlavorTextEntries = [],
+      Habitat = habitatDto,
+    };
+
+    _httpClientMock
+      .Setup(m => m.GetPokemonByNameAsync(new Name(name), cancellationToken))
+      .ReturnsAsync(pokemonApiResponse, TimeSpan.FromMilliseconds(value: 10));
+
+    // ACT
+    var result = await _sut.GetByNameAsync(new Name(name), cancellationToken);
+
+    // ASSERT
+    result.Should().NotBeNull();
+    result.HasValue.Should().BeTrue();
+
+    // check result value
+    var pokemon = result.Value;
+
+    pokemon.Should().NotBeNull();
+    pokemon.Description.Should().BeNull();
+  }
+
+  [Theory]
+  [AutoData]
+  public async Task GetPokemonByNameAsync_Handles_FlavorTextEntries_List_With_Languages_Other_Than_English(
+    string name,
+    bool isLegendary,
+    string habitat,
+    string flavorText,
+    CancellationToken cancellationToken)
+  {
+    // ARRANGE
+    var habitatDto = new HabitatDto { Name = habitat };
+
+    var flavorTextDto = new FlavorTextDto
+    {
+      FlavorText = flavorText,
+      Language = new LanguageDto { Name = "it" },
+    };
+
+    var pokemonApiResponse = new PokemonApiResponse
+    {
+      IsLegendary = isLegendary,
+      Name = name,
+      FlavorTextEntries = [flavorTextDto],
+      Habitat = habitatDto,
+    };
+
+    _httpClientMock
+      .Setup(m => m.GetPokemonByNameAsync(new Name(name), cancellationToken))
+      .ReturnsAsync(pokemonApiResponse, TimeSpan.FromMilliseconds(value: 10));
+
+    // ACT
+    var result = await _sut.GetByNameAsync(new Name(name), cancellationToken);
+
+    // ASSERT
+    result.Should().NotBeNull();
+    result.HasValue.Should().BeTrue();
+
+    // check result value
+    var pokemon = result.Value;
+
+    pokemon.Should().NotBeNull();
+    pokemon.Description.Should().BeNull();
+  }
+
+  [Theory]
+  [AutoData]
+  public async Task GetPokemonByNameAsync_Set_Description_Value_Using_First_Availabler_English_FlavorText(
+    string name,
+    bool isLegendary,
+    string habitat,
+    string flavorText1,
+    string flavorText2,
+    string flavorText3,
+    CancellationToken cancellationToken)
+  {
+    // ARRANGE
+    var habitatDto = new HabitatDto { Name = habitat };
+
+    var flavorTextDto1 = new FlavorTextDto
+    {
+      FlavorText = flavorText1,
+      Language = new LanguageDto { Name = "it" },
+    };
+
+    var flavorTextDto2 = new FlavorTextDto
+    {
+      FlavorText = flavorText2,
+      Language = new LanguageDto { Name = CultureCodes.English },
+    };
+
+    var flavorTextDto3 = new FlavorTextDto
+    {
+      FlavorText = flavorText3,
+      Language = new LanguageDto { Name = CultureCodes.English },
+    };
+
+    var pokemonApiResponse = new PokemonApiResponse
+    {
+      IsLegendary = isLegendary,
+      Name = name,
+      FlavorTextEntries = [flavorTextDto1, flavorTextDto2, flavorTextDto3],
+      Habitat = habitatDto,
+    };
+
+    _httpClientMock
+      .Setup(m => m.GetPokemonByNameAsync(new Name(name), cancellationToken))
+      .ReturnsAsync(pokemonApiResponse, TimeSpan.FromMilliseconds(value: 10));
+
+    // ACT
+    var result = await _sut.GetByNameAsync(new Name(name), cancellationToken);
+
+    // ASSERT
+    result.Should().NotBeNull();
+    result.HasValue.Should().BeTrue();
+
+    // check result value
+    var pokemon = result.Value;
+
+    pokemon.Should().NotBeNull();
+    pokemon.Description.Should().Be(flavorText2);
+  }
 }
