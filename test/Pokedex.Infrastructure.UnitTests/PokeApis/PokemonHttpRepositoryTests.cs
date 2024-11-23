ecef727dc7de68db66ed21dadff8afe802bd6fb2
Enrico Massone <enrico.massone@hotmail.it>
2024-11-23 19:06:03 +0100

    feat: test in progress

--- test/Pokedex.Infrastructure.UnitTests/PokeApis/PokemonHttpRepositoryTests.cs
+++ test/Pokedex.Infrastructure.UnitTests/PokeApis/PokemonHttpRepositoryTests.cs
@@ -1,5 +1,8 @@
-﻿using FluentAssertions;
+﻿using AutoFixture.Xunit2;
+using FluentAssertions;
 using Moq;
+using Pokedex.Domain.Abstractions;
+using Pokedex.Domain.Pokemons;
 using Pokedex.Infrastructure.PokeApis;
 
 namespace Pokedex.Infrastructure.UnitTests.PokeApis;
@@ -25,5 +28,71 @@ public sealed class PokemonHttpRepositoryTests
 
     // ASSERT
     exception.ParamName.Should().Be("name");
+  }
+
+  [Theory]
+  [AutoData]
+  public async Task GetPokemonByNameAsync_Returns_Pokemon_Based_On_Http_Client_Response(
+    string name,
+    bool isLegendary,
+    string habitat,
+    string flavorText,
+    CancellationToken cancellationToken)
+  {
+    // ARRANGE
+    var habitatDto = new HabitatDto { Name = habitat };
+
+    var flavorTextDto = new FlavorTextDto
+    {
+      FlavorText = flavorText,
+      Language = new LanguageDto { Name = CultureCodes.English },
+    };
+
+    var pokemonApiResponse = new PokemonApiResponse
+    {
+      IsLegendary = isLegendary,
+      Name = name,
+      FlavorTextEntries = [flavorTextDto],
+      Habitat = habitatDto,
+    };
+
+    _httpClientMock
+      .Setup(m => m.GetPokemonByNameAsync(new Name(name), cancellationToken))
+      .ReturnsAsync(pokemonApiResponse, TimeSpan.FromMilliseconds(value: 10));
+
+    // ACT
+    var result = await _sut.GetByNameAsync(new Name(name), cancellationToken);
+
+    // ASSERT
+    result.Should().NotBeNull();
+    result.HasValue.Should().BeTrue();
+
+    // check result value
+    var pokemon = result.Value;
+
+    pokemon.Should().NotBeNull();
+    pokemon.Name.Should().Be(new Name(name));
+    pokemon.IsLegendary.Should().Be(isLegendary);
+    pokemon.Habitat.Should().Be(habitat);
+    pokemon.Description.Should().Be(flavorText);
+  }
+
+  [Theory]
+  [AutoData]
+  public async Task GetPokemonByNameAsync_Returns_Empty_Option_When_Http_Client_Returns_Empty_Option(
+    string name,
+    CancellationToken cancellationToken)
+  {
+    // ARRANGE
+    _httpClientMock
+      .Setup(m => m.GetPokemonByNameAsync(new Name(name), cancellationToken))
+      .ReturnsAsync(Option<PokemonApiResponse>.None, TimeSpan.FromMilliseconds(value: 10));
+
+    // ACT
+    var result = await _sut.GetByNameAsync(new Name(name), cancellationToken);
+
+    // ASSERT
+    result.Should().NotBeNull();
+    result.HasValue.Should().BeFalse();
   }
 }
