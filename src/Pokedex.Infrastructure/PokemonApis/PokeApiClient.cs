93c9f1ca877b3316bdc119103b9c6eebf150937d
Enrico Massone <enrico.massone@hotmail.it>
2024-11-23 10:51:55 +0100

    feat: defined dto PokeApiResponse

---
+++ src/Pokedex.Infrastructure/PokemonApis/PokeApiClient.cs
@@ -0,0 +1,18 @@
+﻿using Pokedex.Domain.Pokemons;
+
+namespace Pokedex.Infrastructure.PokemonApis;
+
+public sealed class PokeApiClient
+{
+  private readonly HttpClient _httpClient;
+
+  public PokeApiClient(HttpClient httpClient)
+  {
+    _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+  }
+
+  public async Task<Pokemon> GetPokemonByNameAsync(string name, CancellationToken cancellationToken)
+  {
+    throw new NotImplementedException();
+  }
+}
