4faee8fcb29531d20aa7c61eccd9068e5bbfa93d
Enrico Massone <enrico.massone@hotmail.it>
2024-11-23 16:38:55 +0100

    feat: wip

---
+++ test/Pokedex.Infrastructure.UnitTests/PokeApis/PokeApiHttpClientTests.cs
@@ -0,0 +1,14 @@
+﻿using AutoFixture.Xunit2;
+
+namespace Pokedex.Infrastructure.UnitTests.PokeApis;
+
+public sealed class PokeApiHttpClientTests
+{
+  [Theory]
+  [AutoData]
+  public async Task GetPokemonByNameAsync_Throws_ArgumentNullException_When_Name_Is_Null(
+    CancellationToken cancellationToken)
+  {
+
+  }
+}
