b261e07a7df1b9366f0c9d0a797484ddacd5eab4
Enrico Massone <enrico.massone@hotmail.it>
2024-11-23 17:31:06 +0100

    feat: testing is in progress

--- test/Pokedex.Infrastructure.UnitTests/PokeApis/PokeApiHttpClientTests.cs
+++ test/Pokedex.Infrastructure.UnitTests/PokeApis/PokeApiHttpClientTests.cs
@@ -1,15 +1,81 @@
-﻿using AutoFixture.Xunit2;
+﻿using FluentAssertions;
+using Pokedex.Domain.Pokemons;
+using Pokedex.Infrastructure.PokeApis;
+using RichardSzalay.MockHttp;
 
 namespace Pokedex.Infrastructure.UnitTests.PokeApis;
 
 public sealed class PokeApiHttpClientTests
 {
-  [Theory]
-  [AutoData]
-  public async Task GetPokemonByNameAsync_Throws_ArgumentNullException_When_Name_Is_Null(
-    CancellationToken cancellationToken)
+  [Fact]
+  public async Task GetPokemonByNameAsync_Throws_ArgumentNullException_When_Name_Is_Null()
   {
-    var filePath = Path.Combine("TestApiResponses", "PokemonSpeciesApiResponse.json");
-    var jsonContent = File.ReadAllText(filePath);
+    // ARRANGE
+    var json = Helpers.ReadTestApiResponseFile(fileName: "PokemonSpeciesApiResponse.json");
+
+    using var mockHttp = new MockHttpMessageHandler();
+
+    mockHttp.When("api/v2/pokemon-species/*")
+            .Respond("application/json", json);
+
+    using var httpClient = mockHttp.ToHttpClient();
+
+    var sut = new PokeApiHttpClient(httpClient);
+
+    // ACT
+    var exception = await Assert.ThrowsAsync<ArgumentNullException>(
+      () => sut.GetPokemonByNameAsync(null!, CancellationToken.None)
+    );
+
+    // ASSERT
+    exception.ParamName.Should().Be("name");
+  }
+
+  [Fact]
+  public async Task GetPokemonByNameAsync_Returns_Non_Empty_Option_When_PokeApi_Returns_200()
+  {
+    // ARRANGE
+    var json = Helpers.ReadTestApiResponseFile(fileName: "PokemonSpeciesApiResponse.json");
+
+    using var mockHttp = new MockHttpMessageHandler();
+
+    mockHttp.Expect("https://server.example.com/api/v2/pokemon-species/bulbasaur")
+            .Respond("application/json", json);
+
+    using var httpClient = mockHttp.ToHttpClient();
+
+    httpClient.BaseAddress = new Uri("https://server.example.com/");
+
+    var sut = new PokeApiHttpClient(httpClient);
+
+    // ACT
+    var result = await sut.GetPokemonByNameAsync(new Name("bulbasaur"), CancellationToken.None);
+
+    // ASSERT
+    result.Should().NotBeNull();
+    result.HasValue.Should().BeTrue();
+
+    // check result value
+    var pokemon = result.Value;
+
+    pokemon.Name.Should().Be("bulbasaur");
+
+    pokemon.IsLegendary.Should().BeFalse();
+
+    pokemon.Habitat.Should().NotBeNull();
+    pokemon.Habitat!.Name.Should().Be("grassland");
+
+    pokemon.FlavorTextEntries.Should().NotBeNull().And.HaveCount(2);
+    pokemon.FlavorTextEntries[0].Should().NotBeNull();
+    pokemon.FlavorTextEntries[0].FlavorText.Should().Be("A strange seed was\nplanted on its\nback at birth.\fThe plant sprouts\nand grows with\nthis POKéMON.");
+    pokemon.FlavorTextEntries[0].Language.Should().NotBeNull();
+    pokemon.FlavorTextEntries[0].Language.Name.Should().Be("en");
+    pokemon.FlavorTextEntries[1].Should().NotBeNull();
+    pokemon.FlavorTextEntries[1].FlavorText.Should().Be("A Bulbasaur es fácil verle echándose una siesta al sol.\nLa semilla que tiene en el lomo va creciendo cada vez más\na medida que absorbe los rayos del sol.");
+    pokemon.FlavorTextEntries[1].Language.Should().NotBeNull();
+    pokemon.FlavorTextEntries[1].Language.Name.Should().Be("es");
+
+    // check executed HTTP requests
+    mockHttp.VerifyNoOutstandingExpectation();
   }
 }
