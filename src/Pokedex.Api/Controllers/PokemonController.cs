d6a2385b0e49be5df6411d7e8ef7d2921f36f520
Enrico Massone <enrico.massone@hotmail.it>
2024-11-24 17:49:20 +0100

    feat: defined endpoint to fetch translated version of a Pokemon

--- src/Pokedex.Api/Controllers/PokemonController.cs
+++ src/Pokedex.Api/Controllers/PokemonController.cs
@@ -1,6 +1,7 @@
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Pokedex.Application.Pokemons.GetPokemon;
+using Pokedex.Application.Translations.GetTranslatedPokemon;
 using Pokedex.Domain.Pokemons;
 using System.ComponentModel.DataAnnotations;
 namespace Pokedex.Api.Controllers;
@@ -35,6 +36,30 @@ public class PokemonController : ControllerBase
     CancellationToken cancellationToken)
   {
     var query = new GetPokemonQuery(new Name(name));
+
+    var response = await _sender.Send(query, cancellationToken);
+
+    return response.HasValue ? response.Value : this.NotFound();
+  }
+
+  /// <summary>
+  /// Gets the translated version of a Pokemon by name
+  /// </summary>
+  /// <param name="name">The name of the Pokemon to fetch</param>
+  /// <param name="cancellationToken">Framework provided <see cref="CancellationToken"/> object</param>
+  /// <returns>Information about the fetched Pokemon</returns>
+  /// <response code="200">Returns an object containing information about the fetched Pokemon</response>
+  /// <response code="404">If the Pokemon being fetched by name does not exist</response>
+  /// <response code="400">If the provided Pokemon name is null or white space</response>
+  [HttpGet("translated/{name}")]
+  [ProducesResponseType(StatusCodes.Status200OK)]
+  [ProducesResponseType(StatusCodes.Status400BadRequest)]
+  [ProducesResponseType(StatusCodes.Status404NotFound)]
+  public async Task<ActionResult<GetTranslatedPokemonQueryResponse>> GetTranslatedByName(
+    [Required(ErrorMessage = "Pokemon name cannot be null or white space")] string name,
+    CancellationToken cancellationToken)
+  {
+    var query = new GetTranslatedPokemonQuery(new Name(name));
 
     var response = await _sender.Send(query, cancellationToken);
 
