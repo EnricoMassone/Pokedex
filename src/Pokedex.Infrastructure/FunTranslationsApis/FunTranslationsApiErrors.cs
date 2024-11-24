d0eedcdd0ec9c334ac8c6a828e4b04bfd8c74610
Enrico Massone <enrico.massone@hotmail.it>
2024-11-24 10:41:50 +0100

    feat: defined errors

---
+++ src/Pokedex.Infrastructure/FunTranslationsApis/FunTranslationsApiErrors.cs
@@ -0,0 +1,10 @@
+﻿using Pokedex.Domain.Abstractions;
+
+namespace Pokedex.Infrastructure.FunTranslationsApis;
+
+public static class FunTranslationsApiErrors
+{
+  public static readonly Error TooManyRequests = new(
+        "FunTranslationsApi.TooManyRequests",
+        "FunTranslationsApi rate limit threshold has been reached");
+}
