cb7e68f91da3c8768a32b6b8647857b211726b4b
Enrico Massone <enrico.massone@hotmail.it>
2024-11-24 10:19:10 +0100

    feat: added configuration

---
+++ src/Pokedex.Infrastructure/FunTranslationsApis/Configuration/FunTranslationsApiOptions.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Pokedex.Infrastructure.FunTranslationsApis.Configuration;
+
+public sealed class FunTranslationsApiOptions
+{
+  public const string FunTranslationsApi = "FunTranslationsApi";
+
+  [Required(ErrorMessage = "FunTranslationsApi:BaseAddress is mandatory configuration")]
+  public Uri BaseAddress { get; init; } = default!;
+}
