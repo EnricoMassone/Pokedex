ca77892ddea940a735579824adc34278ef89048d
Enrico Massone <enrico.massone@hotmail.it>
2024-11-24 15:26:40 +0100

    feat: implemented class GetTranslatedPokemonQueryHandler

---
+++ src/Pokedex.Application/Translations/GetTranslatedPokemon/GetTranslatedPokemonQuery.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using Pokedex.Domain.Abstractions;
+using Pokedex.Domain.Pokemons;
+
+namespace Pokedex.Application.Translations.GetTranslatedPokemon;
+
+public sealed record GetTranslatedPokemonQuery(Name Name) : IRequest<Option<GetTranslatedPokemonQueryResponse>>;
---
+++ src/Pokedex.Application/Translations/GetTranslatedPokemon/GetTranslatedPokemonQueryHandler.cs
@@ -0,0 +1,41 @@
+﻿using MediatR;
+using Pokedex.Application.Extensions;
+using Pokedex.Application.Translations.GetTranslatedPokemon.Abstractions;
+using Pokedex.Domain.Abstractions;
+using Pokedex.Domain.Pokemons;
+
+namespace Pokedex.Application.Translations.GetTranslatedPokemon;
+
+public sealed class GetTranslatedPokemonQueryHandler : IRequestHandler<GetTranslatedPokemonQuery, Option<GetTranslatedPokemonQueryResponse>>
+{
+  private readonly IPokemonRepository _repository;
+  private readonly IPokemonTranslator _pokemonTranslator;
+
+  public GetTranslatedPokemonQueryHandler(IPokemonRepository repository, IPokemonTranslator pokemonTranslator)
+  {
+    _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+    _pokemonTranslator = pokemonTranslator ?? throw new ArgumentNullException(nameof(pokemonTranslator));
+  }
+
+  public async Task<Option<GetTranslatedPokemonQueryResponse>> Handle(
+    GetTranslatedPokemonQuery request,
+    CancellationToken cancellationToken)
+  {
+    ArgumentNullException.ThrowIfNull(request);
+
+    var optionalPokemon = await _repository.GetByNameAsync(
+      request.Name,
+      cancellationToken).ConfigureAwait(false);
+
+    if (!optionalPokemon.HasValue)
+    {
+      return Option<GetTranslatedPokemonQueryResponse>.None;
+    }
+
+    var translatedPokemon = await _pokemonTranslator.TranslateAsync(
+      optionalPokemon.Value,
+      cancellationToken).ConfigureAwait(false);
+
+    return translatedPokemon.ToGetTranslatedPokemonQueryResponse();
+  }
+}
