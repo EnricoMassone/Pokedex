dac91c86e6493393bba1e3aa5588e90b22e903f1
Enrico Massone <enrico.massone@hotmail.it>
2024-11-24 17:55:03 +0100

    feat: added tests for the new endpoint

--- test/Pokedex.Api.UnitTests/Controllers/PokemonControllerTests.cs
+++ test/Pokedex.Api.UnitTests/Controllers/PokemonControllerTests.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Moq;
 using Pokedex.Api.Controllers;
 using Pokedex.Application.Pokemons.GetPokemon;
+using Pokedex.Application.Translations.GetTranslatedPokemon;
 using Pokedex.Domain.Abstractions;
 using Pokedex.Domain.Pokemons;
 
@@ -60,6 +61,53 @@ public sealed class PokemonControllerTests
 
     // ACT
     var result = await _sut.GetByName(name, cancellationToken);
+
+    // ASSERT
+    result.Should().NotBeNull();
+    result.Value.Should().BeNull();
+    result.Result.Should().NotBeNull();
+    result.Result.Should().BeOfType<NotFoundResult>();
+  }
+
+  [Theory]
+  [AutoData]
+  public async Task GetTranslatedByName_Returns_Pokemon_Value_When_Query_Returns_Non_Empty_Option(
+    Name name,
+    GetTranslatedPokemonQueryResponse response,
+    CancellationToken cancellationToken)
+  {
+    // ARRANGE
+    var query = new GetTranslatedPokemonQuery(name);
+
+    _senderMock
+      .Setup(m => m.Send(query, cancellationToken))
+      .ReturnsAsync(response, TimeSpan.FromMilliseconds(value: 5));
+
+    // ACT
+    var result = await _sut.GetTranslatedByName(name, cancellationToken);
+
+    // ASSERT
+    result.Should().NotBeNull();
+    result.Value.Should().NotBeNull();
+    result.Value.Should().Be(response);
+    result.Result.Should().BeNull();
+  }
+
+  [Theory]
+  [AutoData]
+  public async Task GetTranslatedByName_Returns_NotFound_Action_Result_When_Query_Returns_Empty_Option(
+    Name name,
+    CancellationToken cancellationToken)
+  {
+    // ARRANGE
+    var query = new GetTranslatedPokemonQuery(name);
+
+    _senderMock
+      .Setup(m => m.Send(query, cancellationToken))
+      .ReturnsAsync(Option<GetTranslatedPokemonQueryResponse>.None, TimeSpan.FromMilliseconds(value: 5));
+
+    // ACT
+    var result = await _sut.GetTranslatedByName(name, cancellationToken);
 
     // ASSERT
     result.Should().NotBeNull();
