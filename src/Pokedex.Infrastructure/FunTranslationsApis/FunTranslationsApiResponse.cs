ceddc703f942fadd680bea9ab5f41aecbe97e94d
Enrico Massone <enrico.massone@hotmail.it>
2024-11-24 10:27:12 +0100

    feat: added response object

---
+++ src/Pokedex.Infrastructure/FunTranslationsApis/FunTranslationsApiResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace Pokedex.Infrastructure.FunTranslationsApis;
+
+public sealed record FunTranslationsApiResponse
+{
+  public required ContentsDto Contents { get; init; }
+}
+
+public sealed record ContentsDto
+{
+  public required string Translated { get; init; }
+}
