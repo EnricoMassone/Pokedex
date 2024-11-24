193a99e0e5ba11dfbc31fdd5a2cfe284e5cb1ee8
Enrico Massone <enrico.massone@hotmail.it>
2024-11-24 10:36:16 +0100

    feat: defined HTTP client and interface

--- src/Pokedex.Infrastructure/FunTranslationsApis/FunTranslationsApiHttpClient.cs
+++ src/Pokedex.Infrastructure/FunTranslationsApis/FunTranslationsApiHttpClient.cs
@@ -1,5 +1,20 @@
-﻿namespace Pokedex.Infrastructure.FunTranslationsApis;
+﻿using Pokedex.Domain.Abstractions;
 
-public sealed class FunTranslationsApiHttpClient
+namespace Pokedex.Infrastructure.FunTranslationsApis;
+
+public sealed class FunTranslationsApiHttpClient : IFunTranslationsApiHttpClient
 {
+  public async Task<Result<FunTranslationsApiResponse>> ApplyYodaTranslationAsync(
+    Text text,
+    CancellationToken cancellationToken)
+  {
+    throw new NotImplementedException();
+  }
+
+  public async Task<Result<FunTranslationsApiResponse>> ApplyShakespeareTranslationAsync(
+    Text text,
+    CancellationToken cancellationToken)
+  {
+    throw new NotImplementedException();
+  }
 }
